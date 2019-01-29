98b39f4fc12eed12c60ec8275bfa6b63ec47e0b5
Prof. ElKharboutly <ruby.elkharboutly@qu.edu>
2019-01-29 13:13:17 -0500

    Spinner implemented

--- app/src/main/java/edu/quinnipiac/ser210/ls02_calculator/CalcActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/ls02_calculator/CalcActivity.java
@@ -3,8 +3,10 @@ package edu.quinnipiac.ser210.ls02_calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class CalcActivity extends AppCompatActivity {
@@ -25,17 +27,65 @@ public class CalcActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(input1.getText() != null && input2.getText() != null){
-                     double num1 = Double.valueOf(input1.getText().toString());
-                    double num2 = Double.valueOf(input2.getText().toString());
-                    double res = num1 + num2;
-                    result.setText(Double.toString(res));
+                    performOp('+');
 
                 }
             }
         });
+        Spinner opSpinner = (Spinner) findViewById(R.id.spinner);
+        opSpinner.setSelection(0,false);
+        opSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String operation = (String) adapterView.getItemAtPosition(i);
+                if (operation.equals("*")){
+                    performOp('*');
+                }
+                if (operation.equals("/")) {
+                    performOp('/');
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
-    public void subtract(View view){
+    // handling event subtract as defined in activity_calc.xml
+    public void subtract(View v){
+        performOp('-');
+    }
+    private void performOp(char op){
+        if ((input1.getText() != null) && (input2.getText() != null) ) {
 
+            // read from input1 and input2
+            double num1 = Double.valueOf(input1.getText().toString());
+            double num2 = Double.valueOf(input2.getText().toString());
+
+            double res = 0;
+
+            // peform operation the two values
+
+            switch (op) {
+                case '+':
+                    res = num1 + num2;
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    break;
+                case '/':
+                    res = num1 / num2;
+                    break;
+
+            }
+            //update the value of text in the result textView
+            result.setText(Double.toString(res));
+        }
     }
 }
